35483d7b12480d85afe00367339b5512672abd2e
Muyi <38084269+SongmyHallow@users.noreply.github.com>
2019-04-08 15:34:43 -0400

    update

---
+++ RectCover/TestDriver.java
@@ -0,0 +1,12 @@
+package RectCover;
+
+public class TestDriver {
+
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        MySolution test = new MySolution();
+        int sol = test.RectCover(10);
+        System.out.println(sol);
+    }   
+
+}
---
+++ RotateArray/MySolution.java
@@ -0,0 +1,16 @@
+package RotateArray;
+import java.util.ArrayList;
+public class MySolution {
+	public int minNumberInRotateArray(int [] array) {
+		if(array.length==0) {
+			return 0;
+		}
+		if(array.length==1) {
+			return array[0];
+		}
+		for(int i=0; i<array.length; i++) {
+			if(array[i+1]<=array[i])	return array[i];
+		}
+		
+	}
+}
