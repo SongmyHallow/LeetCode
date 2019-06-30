d4f638718636feca38910f9163024d78e09d6d91
Muyi <38084269+SongmyHallow@users.noreply.github.com>
2019-06-30 17:32:02 -0400

    sync

--- JumpCrazyFloors/TestDriver.java
+++ JumpCrazyFloors/TestDriver.java
@@ -5,7 +5,7 @@ public class TestDriver {
     public static void main(String[] args) {
         // TODO Auto-generated method stub
         MySolution test = new MySolution();
-        int sol = test.JumpFloorII(4);
+        int sol = test.JumpFloorII(14);
         System.out.println(sol);
     }
 
