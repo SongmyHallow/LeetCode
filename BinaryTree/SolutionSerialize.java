b8bfcfe7fe6f475158cfdf7c315a133490bde5b4
SongmyHallow <songmy1017@gmail.com>
2019-07-16 08:44:56 -0400

    update 1 problem

--- BinaryTree/SolutionSerialize.java
+++ BinaryTree/SolutionSerialize.java
@@ -1,5 +1,25 @@
+import solution.TreeNode;
+
 public class SolutionSerialize{
     String Serialize(TreeNode root){
+        StringBuilder str = new StringBuilder();
+        PreOrder(root, str);
+        return str.toString();
+    }
+
+    private void PreOrder(TreeNode root, StringBuilder str){
+        if(root == null){
+            str.append("#!");
+            return;
+        }
+
+        str.append(root.value+"#");
+        PreOrder(root.left, str);
+        PreOrder(root.right, str);
+    }
+
+    TreeNode Deserialize(String str){
         
     }
+
 }
