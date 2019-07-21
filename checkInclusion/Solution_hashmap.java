268a2c71cf2f4397ac55035957c82fe46f2e2376
Muyi <38084269+SongmyHallow@users.noreply.github.com>
2019-07-21 17:29:09 -0400

    update variant length sliding window problems

--- checkInclusion/Solution_hashmap.java
+++ checkInclusion/Solution_hashmap.java
@@ -12,17 +12,28 @@ public class Solution_hashmap{
 
         for(int i=0; i<=s2.length()-s1.length(); i++){
             HashMap<Character,Integer> s2map = new HashMap<>();
-            for(int j=0; j<s1.length; j++){
+            for(int j=0; j<s1.length(); j++){
                 s2map.put(s2.charAt(i+j), s2map.getOrDefault(s2.charAt(i+j), 0)+1);
             }
-            if()
+            if(match(s1map, s2map)) return true;
         }
+        return false;
     }
-
+    /**
+     * If the two hashmaps obtained are identical for any such window, 
+     * we can conclude that s1's permutation is a substring of s2, otherwise not.
+     */
     public boolean match(HashMap<Character,Integer> s1map, HashMap<Character,Integer> s2map){
         for(char key: s1map.keySet()){
             if(s1map.get(key) - s2map.getOrDefault(key, -1) != 0)   return false;
         }
         return true;
+    }
+
+    public static void main(String[] args) {
+        String s1 = "ab";
+        String s2 = "eidboaoo";
+        Solution_hashmap sol = new Solution_hashmap();
+        System.out.println(sol.checkInclusion(s1, s2));
     }
 }
---
+++ longestSubstring/Solution.java
@@ -0,0 +1,13 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class Solution{
+    public int longestSubstring(String s, int k) {
+        Map<Character,Integer> map = new HashMap<>();
+        
+        for(int i=0; i<s.length(); i++){
+            
+        }
+        
+    }
+}
